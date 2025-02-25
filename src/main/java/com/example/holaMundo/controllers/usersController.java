555be1c1f9d06fa3c8f5e7c633aad5ac993d042e
Jesua <jesuasantes@gmail.com>
2025-02-25 00:07:00 -0500

    Mensaje de Cambio

--- src/main/java/com/example/holaMundo/controllers/usersController.java
+++ src/main/java/com/example/holaMundo/controllers/usersController.java
@@ -15,6 +15,7 @@ public class usersController {
     public String home (Model model){
         model.addAttribute("mensaje", "Hola Mundo en la WEB");
         model.addAttribute("mensaje1", "Prueba de GIT");
+        model.addAttribute("mensaje2", "Prueba de Jesuá");
         return "index"; //retorna el nombre de la vista en este caso index.html
     }
 
